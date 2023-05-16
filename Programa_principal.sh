0495b3b5536e845d6bfb3d3d35eb7f79066b603b
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2023-05-16 12:44:29 +0200

    dia 16 mayo

--- Programa_principal.sh
+++ Programa_principal.sh
@@ -4,11 +4,19 @@
 #Enlazar programa funcional con el de funciones
 . ./funciones.sh
 
-#1.Comprobar si tenemos conexion a internet 
+#1. Comprobar que esta la cableada correctamente configurada
+f_estado_cableada
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo 'El nombre de la tarjeta es: '$nombre_tarjeta_cableada
+
+
+#. Comprobar si tenemos conexion a internet 
 f_conexion
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo '1 ok --------------------------------------------------------------'
-
+echo 'Usted tiene conectividad con la red.'
--- funciones.sh
+++ funciones.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 #Fichero de funciones
 
-#Función 1: comprobamos la conectividad
-function f_conexion {
-    if ping -c 1 -q 8.8.8.8 > /dev/null; then
-    return 0
+
+#Función 1: comprobamos que la tarjeta cableada si esta UP o DOWN y si existe
+function f_estado_cableada {
+    nombre_tarjeta_cableada=$(ip link | awk '/\<UP\>/ {sub(/:$/, "", $2); if ($2 ~ /^e/ && $9 == "UP") print $2}' | grep -e '^e')
+    if [ $? ] ; then
+        return 0
     else
-    echo -e "Para ejecutar este script es necesario que disponga de conexion a internet"
+        echo -e "La tarjeta cableada no ha sido encontrada."
     return 1
     fi
 }
 
-#Función 2: comprobamos que la tarjeta cableada si esta UP o DOWN
-function f_estado_cableada {
 
+#Función 2: comprobamos el estado de la tarjeta de WIFI.
+
+#Función : comprobamos la conectividad
+function f_conexion {
+    if ping -c 1 -q 8.8.8.8 > /dev/null; then
+        return 0
+    else
+        echo -e "Para ejecutar este script es necesario que disponga de conexion a internet."
+        echo -e "Para ello, deberá tener la cableada subida y perfectamente configurada."
+    return 1
+    fi
 }
