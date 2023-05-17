1b8538df12992a27f58ce477f73c5f2e971b0214
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2023-05-17 12:52:23 +0200

    mejoras

--- Programa_principal.sh
+++ Programa_principal.sh
@@ -9,14 +9,17 @@ f_estado_cableada
 if [ $? -ne 0 ]; then
     exit 1
 fi
+echo 'El nombre de la tarjeta cableada es: '$nombre_tarjeta_cableada
 
-echo 'El nombre de la tarjeta es: '$nombre_tarjeta_cableada
-
-
-#. Comprobar si tenemos conexion a internet 
-f_conexion
+f_estado_inalambrica
 if [ $? -ne 0 ]; then
     exit 1
 fi
+echo 'El nombre de la tarjeta WIFI es: '$nombre_tarjeta_wifi
 
-echo 'Usted tiene conectividad con la red.'
+#. Comprobar si tenemos conexion a internet 
+#f_conexion
+#if [ $? -ne 0 ]; then
+#    exit 1
+#fi
+#echo 'Usted tiene conectividad con la red.'
--- funciones.sh
+++ funciones.sh
@@ -4,17 +4,28 @@
 
 #Función 1: comprobamos que la tarjeta cableada si esta UP o DOWN y si existe
 function f_estado_cableada {
-    nombre_tarjeta_cableada=$(ip link | awk '/\<UP\>/ {sub(/:$/, "", $2); if ($2 ~ /^e/ && $9 == "UP") print $2}' | grep -e '^e')
-    if [ $? ] ; then
+    nombre_tarjeta_cableada=$(ip link | awk '{sub(/:$/, "", $2); if ($2 ~ /^e/) print $2}' | grep -e '^e')
+    
+    if [ $(ip link | awk '/\<UP\>/ {sub(/:$/, "", $2); if ($2 ~ /^e/ && $9 == "UP") print $2}' | grep -e '^e') ] ; then
         return 0
     else
-        echo -e "La tarjeta cableada no ha sido encontrada."
+        echo -e "La tarjeta cableada $nombre_tarjeta_cableada no ha sido encontrada."
+        echo -e "¿Quiere usted subir la tarjeta? [Y/N]"
     return 1
     fi
 }
 
-
 #Función 2: comprobamos el estado de la tarjeta de WIFI.
+function f_estado_inalambrica {
+    nombre_tarjeta_wifi=$(ip link | awk '/\<UP\>/ {sub(/:$/, "", $2); if ($2 ~ /^w/) print $2}' | grep -e '^w')
+    if [ $? ] ; then
+        return 0
+    else
+        echo -e "La tarjeta WIFI no ha sido encontrada."
+    return 1
+    fi
+}
+
 
 #Función : comprobamos la conectividad
 function f_conexion {
